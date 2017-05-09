56ae60435506dc6f5cb22ceaad785e5df7cbddb9
Kerwin So <kerwinso@rice.edu>
2017-05-09 11:07:41 -0500

    create regression-testing folder and files

---
+++ regression-testing/NavigateToAllSubjectsFromHomepage.py
@@ -0,0 +1,53 @@
+from pprint import pprint
+from testrail import *
+import os
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+# Testrail API credentials
+client = APIClient('https://openstax.testrail.net')
+client.user = os.environ['TESTRAIL_USER']
+client.password = os.environ['TESTRAIL_KEY']
+
+url = 'https://oscms-qa.openstax.org/'
+
+driver = webdriver.Chrome()
+driver.execute_script('window.focus()')
+driver.set_window_position(1280, 0)  # fullscreen:1280|laptop:720
+driver.set_window_size(1280, 1360)  # fullscreen:1280,1360|laptop:720,800
+driver.get(url)
+
+wait = WebDriverWait(driver, 10)
+
+eas_button = wait.until(
+                EC.element_to_be_clickable(
+                        (By.CLASS_NAME, 'explore-all')
+                )
+              )
+
+eas_button.click()
+
+# xpath for AP microecon book cover == functional test of all content loading on the page
+xpath_apmicro = "//div/div[3]/div[1]/div/div[5]/div/div[3]/h3/img"
+
+# look for the AP Microecon book cover
+apmicro = wait.until(
+                EC.presence_of_element_located(
+                        (By.XPATH, xpath_apmicro)
+                )
+              )
+
+if apmicro:
+    print (driver.current_url + ': PASS, found cover img for Principles of AP Microeconomics')
+    client.send_post(
+        'add_result/130344',
+        {'status_id':1, 'comment': 'Passed (Testrail)'}
+    )
+    results = client.send_get('get_results/130344&limit=1')
+    pprint (results)
+else:
+    print (driver.current_url + ': Fail, cover img for Principles of AP Microeconomics not found')
+
+driver.quit()
